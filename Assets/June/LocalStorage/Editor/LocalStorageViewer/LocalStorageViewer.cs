f072d046db0b0359d84c3b28f8d00079f68a7380
Gaurang Sinha <gaurang@junesoftware.com>
2015-11-02 19:40:39 +0530

    Refactored Local Storage Editor Window rendering.

--- Assets/June/LocalStorage/Editor/LocalStorageViewer/LocalStorageViewer.cs
+++ Assets/June/LocalStorage/Editor/LocalStorageViewer/LocalStorageViewer.cs
@@ -7,7 +7,11 @@ using June;
 
 using Logger = UnityEngine.Debug;
 
-namespace JuneTools { 
+namespace JuneTools {
+
+	/// <summary>
+	/// Local storage editor window.
+	/// </summary>
 	public class LocalStorageViewer : EditorWindow {
 	
 		public List<LocalStorageValue> VALUES = null;
@@ -15,7 +19,7 @@ namespace JuneTools {
 		private bool _IsCreatingNew;
 		private LocalStorageValue _NewValue;
 
-	#region UI Methods
+		#region UI Methods
 		private GUIStyle _BoxStyle;
 		/// <summary>
 		/// Gets the box style.
@@ -61,7 +65,7 @@ namespace JuneTools {
 			texture.Apply ();
 			return texture;
 		}
-	#endregion
+		#endregion
 
 		[MenuItem ("June/Local Storage Viewer")]	
 		private static void Init() {
@@ -75,106 +79,112 @@ namespace JuneTools {
 				RefreshData ();
 			}
 
-			// --------------------------------------------
 			// Toolbar
-			// --------------------------------------------
-			GUILayout.BeginHorizontal (EditorStyles.toolbar);
-			if (GUILayout.Button ("Create New", EditorStyles.toolbarButton)) {
-				this._NewValue = new LocalStorageValue(new KeyValuePair<string, object>(string.Empty, 0));
-				this._IsCreatingNew = true;
+			RenderToolbar ();
+
+			// Create New
+			if (this._IsCreatingNew) {
+				RenderCreateNew ();	
 			}
 		
+			// Displays current local store provider type.
+			GUILayout.Label ("Local Storage Values for : " + LocalStore.Instance.ToString ());
+		
+			// Render stored values
+			RenderValues ();
+		}
+
+		/// <summary>
+		/// Renders the toolbar.
+		/// </summary>
+		private void RenderToolbar () {
+			GUILayout.BeginHorizontal (EditorStyles.toolbar);
+			if (GUILayout.Button ("Create New", EditorStyles.toolbarButton)) {
+				this._NewValue = new LocalStorageValue (new KeyValuePair<string, object> (string.Empty, 0));
+				this._IsCreatingNew = true;
+			}
 			GUILayout.FlexibleSpace ();
-			if(GUILayout.Button("Print", EditorStyles.toolbarButton)) {
-				DebugPrint(this.VALUES);
+			if (GUILayout.Button ("Print", EditorStyles.toolbarButton)) {
+				DebugPrint (this.VALUES);
 			}
 			if (GUILayout.Button ("Save All", EditorStyles.toolbarButton)) {
 				SaveAll ();
 				RefreshData ();
 			}
-		
 			if (GUILayout.Button ("Refresh/Restore", EditorStyles.toolbarButton)) {
 				RefreshData ();
 			}
-
-			if(GUILayout.Button("DELETE ALL DATA", EditorStyles.toolbarButton)) {
-				if(EditorUtility.DisplayDialog ("Delete All Data", "Are you sure you want to delete all the data?", "YES", "NO")) {
-					LocalStore.Instance.DeleteAll();
-					RefreshData();
+			if (GUILayout.Button ("DELETE ALL DATA", EditorStyles.toolbarButton)) {
+				if (EditorUtility.DisplayDialog ("Delete All Data", "Are you sure you want to delete all the data?", "YES", "NO")) {
+					LocalStore.Instance.DeleteAll ();
+					RefreshData ();
 				}
 			}
-
 			GUILayout.EndHorizontal ();
+		}
 
-
-			// --------------------------------------------
-			// Create New
-			// --------------------------------------------
-			if (this._IsCreatingNew) {
-
-				if (null == this._NewValue) {
-					this._NewValue = new LocalStorageValue(new KeyValuePair<string, object>(string.Empty, 0f));
+		/// <summary>
+		/// Renders the create new section.
+		/// </summary>
+		void RenderCreateNew () {
+			if (null == this._NewValue) {
+				this._NewValue = new LocalStorageValue (new KeyValuePair<string, object> (string.Empty, 0f));
+			}
+			GUILayout.BeginArea (new Rect (5, 20, position.width - 10, 98), BoxStyle);
+			GUILayout.Space (3);
+			GUILayout.Label ("Create New", EditorStyles.boldLabel);
+			this._NewValue.Name = EditorGUILayout.TextField ("Key : ", this._NewValue.Name);
+			GUILayout.BeginHorizontal ();
+			switch (this._NewValue.Type) {
+			case ValueType.Int:
+				this._NewValue.Value = EditorGUILayout.IntField ("Initial Value : ", this._NewValue.IntValue);
+				break;
+			case ValueType.Float:
+				this._NewValue.Value = EditorGUILayout.FloatField ("Initial Value : ", this._NewValue.FloatValue);
+				break;
+			case ValueType.String:
+				this._NewValue.Value = EditorGUILayout.TextField ("Initial Value : ", this._NewValue.StringValue);
+				break;
+			}
+			ValueType valType = (ValueType)EditorGUILayout.EnumPopup (this._NewValue.Type, GUILayout.MaxWidth (80));
+			if (this._NewValue.Type != valType) {
+				Logger.Log ("SETTING FROM:" + this._NewValue.Type + " TO:" + valType);
+				string name = this._NewValue.Name;
+				object obj = string.Empty;
+				if (valType == ValueType.Int) {
+					obj = 0;
 				}
-
-				GUILayout.BeginArea (new Rect (5, 20, position.width - 10, 98), BoxStyle);
-				GUILayout.Space (3);
-				GUILayout.Label ("Create New Pref", EditorStyles.boldLabel);
-				this._NewValue.Name = EditorGUILayout.TextField ("New Pref Name : ", this._NewValue.Name);
-
-				GUILayout.BeginHorizontal ();
-				switch (this._NewValue.Type) {
-					case ValueType.Int:
-						this._NewValue.Value = EditorGUILayout.IntField ("Initial Value : ", this._NewValue.IntValue);
-						break;
-					case ValueType.Float:
-						this._NewValue.Value = EditorGUILayout.FloatField ("Initial Value : ", this._NewValue.FloatValue);
-						break;
-					case ValueType.String:
-						this._NewValue.Value = EditorGUILayout.TextField ("Initial Value : ", this._NewValue.StringValue);
-						break;
-				}
-				ValueType valType = (ValueType)EditorGUILayout.EnumPopup (this._NewValue.Type, GUILayout.MaxWidth (80));
-				if(this._NewValue.Type != valType) {
-					Debug.Log("SETTING FROM:" + this._NewValue.Type + " TO:" + valType);
-					string name = this._NewValue.Name;
-					object obj = string.Empty;
-					if(valType == ValueType.Int) {
-						obj = 0;
-					}
-					else if(valType == ValueType.Float) {
+				else
+					if (valType == ValueType.Float) {
 						obj = 0f;
 					}
-					this._NewValue = new LocalStorageValue(name, obj);
-				}
-				this._NewValue.Type = valType;
-				GUILayout.EndHorizontal ();
-
-				GUILayout.Space (4);
-
-				GUILayout.BeginHorizontal ();
-				GUILayout.FlexibleSpace ();
-				if (GUILayout.Button ("Create")) {
-					if (null == this.VALUES) {
-						this.VALUES = new List<LocalStorageValue>();
-					}
-					this.VALUES.Add (this._NewValue);
-					this.SaveAll ();
-					this._IsCreatingNew = false;
-				}
-			
-				if (GUILayout.Button ("Cancel")) {
-					this._IsCreatingNew = false;
-					this._NewValue = null;
-				}
-				GUILayout.EndHorizontal ();
-				GUILayout.Space (5);
-				GUILayout.EndArea ();
-				GUILayout.Space (104);
+				this._NewValue = new LocalStorageValue (name, obj);
 			}
-		
-		
-			GUILayout.Label ("Local Storage Values for : " + LocalStore.Instance.ToString ());
-		
+			this._NewValue.Type = valType;
+			GUILayout.EndHorizontal ();
+			GUILayout.Space (4);
+			GUILayout.BeginHorizontal ();
+			GUILayout.FlexibleSpace ();
+			if (GUILayout.Button ("Create")) {
+				if (null == this.VALUES) {
+					this.VALUES = new List<LocalStorageValue> ();
+				}
+				this.VALUES.Add (this._NewValue);
+				this.SaveAll ();
+				this._IsCreatingNew = false;
+			}
+			if (GUILayout.Button ("Cancel")) {
+				this._IsCreatingNew = false;
+				this._NewValue = null;
+			}
+			GUILayout.EndHorizontal ();
+			GUILayout.Space (5);
+			GUILayout.EndArea ();
+			GUILayout.Space (104);
+		}
+
+		void RenderValues ()
+		{
 			this._ScrollPosition = GUILayout.BeginScrollView (this._ScrollPosition);
 			if (this.VALUES.Count == 0) {
 				GUILayout.Label ("No local storage values for current project", EditorStyles.miniLabel);
@@ -182,24 +192,23 @@ namespace JuneTools {
 			else {
 				foreach (var val in this.VALUES) {
 					GUILayout.BeginHorizontal (GUILayout.MinHeight (18));
-
 					if (val.IsToBeDeleted) {
 						GUI.color = Color.red;
-					} 
-					else if (val.HasChanged) {
-						GUI.color = Color.yellow;
 					}
-
+					else
+						if (val.HasChanged) {
+							GUI.color = Color.yellow;
+						}
 					switch (val.Type) {
-						case ValueType.Int:
-							val.Value = EditorGUILayout.IntField (val.Name, val.IntValue, EditorStyles.textField, GUILayout.MaxWidth (500));
-							break;
-						case ValueType.Float:
-							val.Value = EditorGUILayout.FloatField (val.Name, val.FloatValue, EditorStyles.textField, GUILayout.MaxWidth (500));
-							break;
-						case ValueType.String:
-							val.Value = EditorGUILayout.TextField (val.Name, val.StringValue, EditorStyles.textField, GUILayout.MaxWidth (500));
-							break;
+					case ValueType.Int:
+						val.Value = EditorGUILayout.IntField (val.Name, val.IntValue, EditorStyles.textField, GUILayout.MaxWidth (500));
+						break;
+					case ValueType.Float:
+						val.Value = EditorGUILayout.FloatField (val.Name, val.FloatValue, EditorStyles.textField, GUILayout.MaxWidth (500));
+						break;
+					case ValueType.String:
+						val.Value = EditorGUILayout.TextField (val.Name, val.StringValue, EditorStyles.textField, GUILayout.MaxWidth (500));
+						break;
 					}
 					GUILayout.FlexibleSpace ();
 					val.Type = (ValueType)EditorGUILayout.EnumPopup (val.Type, GUILayout.MaxWidth (140));
